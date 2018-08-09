102ca03e2621004e327bf65ca5de6f693d1ee75d
luruidi <915628662@qq.com>
2018-08-09 18:32:08 +0800

    add demo and eureka

---
+++ spring-cloud-demo/demo-service/src/main/java/com/example/web/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.web;
+
+import com.example.bean.Book;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class DemoController {
+    @Resource
+    private Book book;
+    @RequestMapping("/demo")
+    public String demo(){
+        System.out.println(book.getName());
+        System.out.println(book.getAuthor());
+        System.out.println(book.getDesc());
+        return "demo";
+    }
+}
