a304acd9574d3d98b9807670c0502f44ecf8da21
luruidi <915628662@qq.com>
2018-08-17 17:21:39 +0800

    update

--- spring-cloud-demo/zuul-server/src/main/java/com/example/ApiGatewayApplication.java
+++ spring-cloud-demo/zuul-server/src/main/java/com/example/ApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import com.example.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world!
@@ -15,5 +17,10 @@ public class ApiGatewayApplication
     public static void main( String[] args )
     {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 }
