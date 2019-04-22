cc7e46dfdc4f0136d912ee1fffc7d2aff77a7ac9
luruidi <915628662@qq.com>
2019-04-22 14:37:16 +0800

    config-server first commit

---
+++ spring-cloud-demo/config-server/src/main/java/com/example/Application.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: luruidi
+ * @date: 2019-04-22 11:32:06
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
