f428a1fdfdfc7fab15975e9fcf9d68506dbb3c89
luruidi <915628662@qq.com>
2019-04-22 15:13:19 +0800

    update

--- spring-cloud-demo/demo-service/src/main/java/com/example/DemoApplication.java
+++ spring-cloud-demo/demo-service/src/main/java/com/example/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableDiscoveryClient
+/*@EnableDiscoveryClient*/
 @SpringBootApplication
+@MapperScan("com.example.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
